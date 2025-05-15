af117d9e0444c286e966c0b5c7b1b79ab804d1ea
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2025-05-16 01:00:52 +0300

    feat: add login page interface and article creation form

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ArticleController extends AbstractController
+{
+    #[Route('/article', name: 'app_article')]
+    public function index(): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }
+
+    #[Route('/article/new', name: 'article_new')]
+    public function new(): Response
+    {
+   
+    }
+}
