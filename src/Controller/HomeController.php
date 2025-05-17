020bee1a96b3e600af17ba65614bfb369adbd537
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2025-05-17 12:17:10 +0300

    feat: add Home, Login, and Signup pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,22 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/login', name: 'app_login')]
+    public function login(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/login.html.twig');
     }
+
+    #[Route('/signup', name: 'app_signup')]
+    public function signup(): Response
+    {
+        return $this->render('home/signup.html.twig');
+    }
+
+    #[Route('/', name:'app_homr')]
+    public function home() : Response
+    {
+        return $this->render('home/home.html.twig');
+    }
+
 }
