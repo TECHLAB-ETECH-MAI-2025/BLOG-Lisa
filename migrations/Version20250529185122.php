f94a83405da26bae64c746a2e3683980e0c95292
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2025-05-29 23:26:21 +0300

    feat: change path homeController && edit access navbar

--- migrations/Version20250529185122.php
+++ migrations/Version20250529185122.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250518140941 extends AbstractMigration
+final class Version20250529185122 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,13 +21,10 @@ final class Version20250518140941 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            CREATE TABLE article (id SERIAL NOT NULL, category_id INT DEFAULT NULL, comment_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, content TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))
+            CREATE TABLE article (id SERIAL NOT NULL, category_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, content TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, image VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))
         SQL);
         $this->addSql(<<<'SQL'
             CREATE INDEX IDX_23A0E6612469DE2 ON article (category_id)
-        SQL);
-        $this->addSql(<<<'SQL'
-            CREATE INDEX IDX_23A0E66F8697D13 ON article (comment_id)
         SQL);
         $this->addSql(<<<'SQL'
             COMMENT ON COLUMN article.created_at IS '(DC2Type:datetime_immutable)'
@@ -36,19 +33,37 @@ final class Version20250518140941 extends AbstractMigration
             COMMENT ON COLUMN article.updated_at IS '(DC2Type:datetime_immutable)'
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE TABLE category (id SERIAL NOT NULL, article_id INT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id))
+            CREATE TABLE category (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id))
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE INDEX IDX_64C19C17294869C ON category (article_id)
-        SQL);
-        $this->addSql(<<<'SQL'
-            CREATE TABLE comment (id SERIAL NOT NULL, article_id INT DEFAULT NULL, author VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))
+            CREATE TABLE comment (id SERIAL NOT NULL, article_id INT NOT NULL, author VARCHAR(255) NOT NULL, content VARCHAR TYPE TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))
         SQL);
         $this->addSql(<<<'SQL'
             CREATE INDEX IDX_9474526C7294869C ON comment (article_id)
         SQL);
         $this->addSql(<<<'SQL'
             COMMENT ON COLUMN comment.created_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE reset_password_request (id SERIAL NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, expires_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_7CE748AA76ED395 ON reset_password_request (user_id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN reset_password_request.requested_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN reset_password_request.expires_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE "user" (id SERIAL NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(100) DEFAULT NULL, last_name VARCHAR(100) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, is_verified BOOLEAN NOT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN "user".created_at IS '(DC2Type:datetime_immutable)'
         SQL);
         $this->addSql(<<<'SQL'
             CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))
@@ -89,13 +104,10 @@ final class Version20250518140941 extends AbstractMigration
             ALTER TABLE article ADD CONSTRAINT FK_23A0E6612469DE2 FOREIGN KEY (category_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE
         SQL);
         $this->addSql(<<<'SQL'
-            ALTER TABLE article ADD CONSTRAINT FK_23A0E66F8697D13 FOREIGN KEY (comment_id) REFERENCES comment (id) NOT DEFERRABLE INITIALLY IMMEDIATE
+            ALTER TABLE comment ADD CONSTRAINT FK_9474526C7294869C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE
         SQL);
         $this->addSql(<<<'SQL'
-            ALTER TABLE category ADD CONSTRAINT FK_64C19C17294869C FOREIGN KEY (article_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE
-        SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE comment ADD CONSTRAINT FK_9474526C7294869C FOREIGN KEY (article_id) REFERENCES article (id) NOT DEFERRABLE INITIALLY IMMEDIATE
+            ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE
         SQL);
     }
 
@@ -109,13 +121,10 @@ final class Version20250518140941 extends AbstractMigration
             ALTER TABLE article DROP CONSTRAINT FK_23A0E6612469DE2
         SQL);
         $this->addSql(<<<'SQL'
-            ALTER TABLE article DROP CONSTRAINT FK_23A0E66F8697D13
-        SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE category DROP CONSTRAINT FK_64C19C17294869C
-        SQL);
-        $this->addSql(<<<'SQL'
             ALTER TABLE comment DROP CONSTRAINT FK_9474526C7294869C
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reset_password_request DROP CONSTRAINT FK_7CE748AA76ED395
         SQL);
         $this->addSql(<<<'SQL'
             DROP TABLE article
@@ -125,6 +134,12 @@ final class Version20250518140941 extends AbstractMigration
         SQL);
         $this->addSql(<<<'SQL'
             DROP TABLE comment
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE reset_password_request
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE "user"
         SQL);
         $this->addSql(<<<'SQL'
             DROP TABLE messenger_messages
--- migrations/Version20250529192017.php
+++ migrations/Version20250529192017.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250521201808 extends AbstractMigration
+final class Version20250529192017 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,9 +20,7 @@ final class Version20250521201808 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql(<<<'SQL'
-            ALTER TABLE article ALTER category_id DROP NOT NULL
-        SQL);
+
     }
 
     public function down(Schema $schema): void
@@ -30,9 +28,6 @@ final class Version20250521201808 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
             CREATE SCHEMA public
-        SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE article ALTER category_id SET NOT NULL
         SQL);
     }
 }
